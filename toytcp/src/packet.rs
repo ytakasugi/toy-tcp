55c6b3d54cca8f79a6484af5810634dd9a20bfa9
ytakasugi <sh7.tibi0129@gmail.com>
2021-12-31 22:46:39 +0900

    add files

---
+++ toytcp/src/packet.rs
@@ -0,0 +1,20 @@
+use crate::tcpflags;
+use pnet::packet::{ip::IpNextHeaderProtocols, tcp::TcpPacket, Packet};
+use pnet::util;
+
+use std::fmt::{self, Debug};
+use std::net::Ipv4Addr;
+const TCP_HEADER_SIZE: usize = 20;
+
+#[derive(Clone)]
+pub struct TCPPacket {
+    buffer: Vec<u8>
+}
+
+impl TCPPacket {
+    pub fn new(payload_len: usize) -> Self {
+        Self {
+            buffer: vec![0; TCP_HEADER_SIZE + payload_len],
+        }
+    }
+}
---
+++ toytcp/src/socket.rs
@@ -0,0 +1,23 @@
+use crate::packet::TCPPacket;
+use crate::tcpflags;
+use anyhow::{Context, Result};
+use pnet::packet::{ip::IpNextHeaderProtocols, Packet};
+use pnet::transport::{self, TransportChannelType, TransportProtocol, TransportSender};
+use pnet::util;
+use std::collections::VecDeque;
+use std::fmt::{self, Display};
+use std::net::{IpAddr, Ipv4Addr};
+use std::time::SystemTime;
+
+const SOCKET_BUFFER_SIZE: usize = 4380;
+
+pub struct SockID();
+
+pub struct Socket {
+}
+
+pub struct RetransmissionQueueEntry {
+}
+
+pub enum TcpStatus {
+}
